0dc762c57758179b111294d25f066eb42e49f7d7
nATOr <gowluonade@gmail.com>
2023-02-02 17:45:07 -0500

    fix

--- src/content/config.ts
+++ src/content/config.ts
@@ -11,5 +11,5 @@ const postsCollection = defineCollection({
 // 3. Export a single `collections` object to register your collection(s)
 //    This key should match your collection directory name in "src/content"
 export const collections = {
-  blog: postsCollection,
+  posts: postsCollection,
 }
