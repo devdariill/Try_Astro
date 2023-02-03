2312104bfbba7d7ebdab19712659a13410ae642c
nATOr <gowluonade@gmail.com>
2023-02-02 19:32:09 -0500

    filter body content

--- src/content/posts/first.md
+++ src/content/posts/first.md
@@ -4,3 +4,12 @@ publishedDate: 2023-01-13
 published: true
 ---
 asdnfoasjdfdjf
+
+## Second heading 
+```js
+let fourtyTwo="the meaning of enerything"
+```
+-a
+-b
+-c
+-<mark>d</mark>
--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,16 +1,23 @@
 import rss from '@astrojs/rss'
 import { getCollection } from 'astro:content'
+// import sanitizeHtml from 'sanitize-html'
+// import MarkdownIt from 'markdown-it'
+// const parser = new MarkdownIt()
 
 export async function get(context) {
-  const posts = await getCollection('posts')
+  const posts = await getCollection('posts', ({ data }) => {
+    return data.published
+  })
+  //   const posts = await getCollection('posts')
   return rss({
     title: 'My Blog',
     description: 'My blog description',
     site: context.site.origin,
-    link: 'https://example.com',
     items: posts.map((post) => ({
       title: post.data.title,
       pubDate: post.data.publishedDate,
+      content: post.body,
+      //   content: sanitizeHtml(parser.render(post.body)),
       link: `/posts/${post.slug}`,
       //   description: post.body,
     })),
